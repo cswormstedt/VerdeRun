ada55eee22c1edaaf5f6e0fb6ea6af8da1abff20
cSwormstedt <cswormstedt7@gmail.com>
2017-03-12 21:44:17 -0500

    added gulp db and app guts

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp  = require('gulp');
+var less  = require('gulp-less');
+var watch = require('gulp-watch');
+
+gulp.task('hello', function() {
+	console.log('yooooo');
+})
+
+gulp.task('watch', function(){
+	gulp.watch(['./server/public/styles/*.less'], ['compile-less']);
+})
+
+gulp.task('compile-less', function(){
+	gulp.src('./server/public/styles/style.less')
+	.pipe(less())
+	.pipe(gulp.dest('./server/public/styles/'))
+})
+
+gulp.task('default', ['compile-less', 'watch'])
