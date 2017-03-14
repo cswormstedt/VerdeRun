95d975c73cab193eacafcb60409d025e88b8f7af
Katelyn Fenchel <katelynfenchel@Katelyns-MacBook-Pro.local>
2017-03-14 10:14:27 -0500

    hi

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,18 +1,18 @@
 console.log('yo');
 //weather api ajax
 
-$.ajax({
-	url: 'https://api.darksky.net/forecast/48fa37f6c40f1efed51b97ee6b3d4671/39.6403,-106.3742',
-	type: 'GET',
-	dataType: 'json',
+// $.ajax({
+// 	url: 'https://api.darksky.net/forecast/48fa37f6c40f1efed51b97ee6b3d4671/39.6403,-106.3742',
+// 	type: 'GET',
+// 	dataType: 'json',
 
-	succes: function(data){
-		console.log(data);
+// 	succes: function(data){
+// 		console.log(data);
 		
-	},
+// 	},
 
-	error: function(error){
-	console.log(error)
-  },
+// 	error: function(error){
+// 	console.log(error)
+//   },
 
-})''
+// })''
