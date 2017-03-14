8f4be7cafac33c4f82b3f033ab4801801ad93f9c
Katelyn Fenchel <katelynfenchel@Katelyns-MacBook-Pro.local>
2017-03-14 14:24:14 -0500

    not sure what i did

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -2,49 +2,41 @@ console.log('yo');
 //weather api ajax
 var response = {};
 
-// $.ajax({
 
-// 	url: 'https://api.darksky.net/forecast/48fa37f6c40f1efed51b97ee6b3d4671/39.6403,-106.3742',
-// 	type: 'GET',
-// 	dataType: 'json',
-
-
-// 	succes: function(data){
-// 		console.log(data);
-		
-// 	},
-
-// 	url: 'http://api.darksky.net/forecast/48fa37f6c40f1efed51b97ee6b3d4671/39.6403,-106.3742',
-// 	type: 'GET',
-// 	dataType: 'json',
-// 	success: function(data){
-// 		console.log(data);
-		
-// 	},
-
-// 	error: function(error){
-// 	console.log(error)
-//   },
-
-// })''
-
-
-// 	succes: function(data){
-// 		response = data;
-
-		
-// 	},
-
-// 	error: function(error){
-// 	console.log(error);
-//   }
-
-// })
 
 $('.signUpOne').on('click',function(e){
 	console.log('click');
 	$('.modalHide').toggleClass('register-modal');
 	$('.signUpOne').hide();
+});
+
+
+$('.place').on('click', function(e){
+	var mountainName = $(e.target).parent();
+	var id = mountainName.data('id');
+	$.ajax({
+		url: '/' + id,
+		type: 'Get',
+		success: function(res){
+			console.log(res);
+		},
+		error: function(err){
+			console.log(err);
+		}
+	})
+});
+
+$.ajax({
+	url: '/profile/',
+	type: "POST",
+	success: function(result){
+		console.log("this is the result");
+	},
+	error: function(err){
+		console.log(err);
+	}
 })
 
-// })
+
+
+
